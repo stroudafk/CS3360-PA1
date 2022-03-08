4f85651d4c34b98f79d16824380626c2f68b8250
sj <stroudafk@gmail.com>
2022-03-08 17:03:50 -0600

    finished implementation of question 1; started question 2 with a bug where the events in A are always greater than the queantity of events in B

--- PA1.cpp
+++ PA1.cpp
@@ -33,9 +33,49 @@ int main(){
   
   for(int i = 1; i <= 1000; i++){
     timeElapsed += -(1.0/2.0) * log(a()); 
-    std::cout << "< " << i << "\t" << timeElapsed << "\t" << std::endl;
+    serviceTime = -(1.0/2.0) * log(1 - a());
+    std::cout << "< " << i << "\t" << timeElapsed << "\t" << serviceTime << " >" << std::endl;
   }
   
+  int yearInHours = 8760;
+  int MTBF = 500;
+  double serverFailureA = 0.0;
+  double serverFailureB = 0.0;
+
+  std::cout << "Server A Failure Times\t" <<  "Restoration Time\t";
+  std::cout << "Server B Failure Times\t" <<  "Restoration Time\t" << std::endl;
+  std::cout << "--------------------------------------------------";
+  std::cout << "--------------------------------------------------" << std::endl;
+
+  std::vector<double> serverATimes;
+  std::vector<double> serverBTimes;
+
+  while(serverFailureA <= 20 * yearInHours || serverFailureB <= 20 * yearInHours){
+  //serverFailureA += -(500) * log(1-a());
+  serverFailureB += -(500) * log(1-a());
+  serverFailureA += -(500) * log(1-a());
+  if(serverFailureA <= 20 * yearInHours){
+    serverATimes.push_back(serverFailureA);
+  }
+  if(serverFailureB <= 20 * yearInHours){
+    serverBTimes.push_back(serverFailureB);
+  }
+  }
+  
+  int i = 0, j = 0;
+  while(i < serverATimes.size() || j < serverBTimes.size()){
+    if(i < serverATimes.size()){
+      std::cout << serverATimes[i] << "\t\t\t";
+      std::cout << serverATimes[i] + 10 << "\t\t\t";
+    }
+    if(j < serverBTimes.size()){
+      std::cout << serverBTimes[j] << "\t\t\t";
+      std::cout << serverBTimes[j] + 10 << "\t\t\t";
+    }
+    i++;
+    j++;
+    std::cout << std::endl;
+  }
 
   return 0;
 }
