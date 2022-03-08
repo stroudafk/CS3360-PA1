2e8f0cce631e98e74788403640098011ee67d4ae
sj <stroudafk@gmail.com>
2022-03-08 17:59:28 -0600

    fixed formatting which gave the appearance of a constant greater number of events in server A than server B

--- PA1.cpp
+++ PA1.cpp
@@ -6,36 +6,24 @@
 double a(){
   return double(rand()) / RAND_MAX;
 }
-double b(){
-  
-  return ((double(rand()) / RAND_MAX) / 4 ) + 0.25;
-}
-double c(){
-  // {{1, 0.5}, {2, 0.2},{x, 0.3}}
-  // on intervals {1, 0.0 < x <= 0.5}, {2, {0.5 > x <= 0.7}},
-  // {3.0-4.0, 0.7 <= x <= 1.0}
-  double result = double(rand()) / RAND_MAX;
-  
-  if(result < 0.5){
-    return 1.0;
-  }
-  else if(result >= 0.5 && result <= 0.7){
-    return 2.0;
-  }
-  else{
-    return ((double(rand()) / RAND_MAX)) + 3; //is there a precision for this?
-  }
-}
 
 int main(){
   srand(time(NULL));
-  double timeElapsed = 0, serviceTime = 0;
-  
+  double timeElapsed = 0, serviceTime = 0, avgServiceTime = 0, avgArrivalRate = 0;
+  double arrivalTime = 0;
+
   for(int i = 1; i <= 1000; i++){
-    timeElapsed += -(1.0/2.0) * log(a()); 
+    timeElapsed += -(1.0/2.0) * log(a());
     serviceTime = -(1.0/2.0) * log(1 - a());
+    avgServiceTime += serviceTime;
     std::cout << "< " << i << "\t" << timeElapsed << "\t" << serviceTime << " >" << std::endl;
   }
+
+  // Calculate averages
+  avgServiceTime /= 1000;
+
+  std::cout << "Average service time: " << avgServiceTime << std::endl;
+  std::cout << "Average arrival rate: " << timeElapsed / 1000 << std::endl;
   
   int yearInHours = 8760;
   int MTBF = 500;
@@ -51,15 +39,15 @@ int main(){
   std::vector<double> serverBTimes;
 
   while(serverFailureA <= 20 * yearInHours || serverFailureB <= 20 * yearInHours){
-  //serverFailureA += -(500) * log(1-a());
-  serverFailureB += -(500) * log(1-a());
-  serverFailureA += -(500) * log(1-a());
-  if(serverFailureA <= 20 * yearInHours){
-    serverATimes.push_back(serverFailureA);
-  }
-  if(serverFailureB <= 20 * yearInHours){
-    serverBTimes.push_back(serverFailureB);
-  }
+    serverFailureA += -(500) * log(1-a());
+    serverFailureB += -(500) * log(1-a());
+
+    if(serverFailureA <= 20 * yearInHours){
+      serverATimes.push_back(serverFailureA);
+    }
+    if(serverFailureB <= 20 * yearInHours){
+      serverBTimes.push_back(serverFailureB);
+    }
   }
   
   int i = 0, j = 0;
@@ -67,6 +55,9 @@ int main(){
     if(i < serverATimes.size()){
       std::cout << serverATimes[i] << "\t\t\t";
       std::cout << serverATimes[i] + 10 << "\t\t\t";
+    }
+    else{
+      std::cout << "\t\t\t\t\t\t";
     }
     if(j < serverBTimes.size()){
       std::cout << serverBTimes[j] << "\t\t\t";
--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 To generate the interarrival times, I used the formula -(1/lambda)(ln(z)),
 where Z is a randomly generated number between 0 and 1, and 2 is lambda.
+-(1/2)(ln(rand()/RAND-MAX))
 
 To generate the service times which follow an exponential distribution, I used the formula -(1/lambda)(ln(1-y)), where y is a random number between 1 and 0.
 
