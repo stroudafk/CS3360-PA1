66af931807f4e3afa623d74ba6599e932e1d6f7e
sj <stroudafk@gmail.com>
2022-03-08 14:37:44 -0600

    seeded rand function once in the main rather than per function. added formula for interrarival times

--- PA1.cpp
+++ PA1.cpp
@@ -1,13 +1,41 @@
 #include <iostream>
 #include <random>
+#include <time.h>
+#include <cmath>
 
+double a(){
+  return double(rand()) / RAND_MAX;
+}
+double b(){
+  
+  return ((double(rand()) / RAND_MAX) / 4 ) + 0.25;
+}
+double c(){
+  // {{1, 0.5}, {2, 0.2},{x, 0.3}}
+  // on intervals {1, 0.0 < x <= 0.5}, {2, {0.5 > x <= 0.7}},
+  // {3.0-4.0, 0.7 <= x <= 1.0}
+  double result = double(rand()) / RAND_MAX;
+  
+  if(result < 0.5){
+    return 1.0;
+  }
+  else if(result >= 0.5 && result <= 0.7){
+    return 2.0;
+  }
+  else{
+    return ((double(rand()) / RAND_MAX)) + 3; //is there a precision for this?
+  }
+}
 
 int main(){
-
+  srand(time(NULL));
+  double timeElapsed = 0, serviceTime = 0;
+  
   for(int i = 1; i <= 1000; i++){
-    std::cout << "< " << i << " " << std::endl;
+    timeElapsed += -(1.0/2.0) * log(a()); 
+    std::cout << "< " << i << "\t" << timeElapsed << "\t" << std::endl;
   }
-
+  
 
   return 0;
 }
---
+++ README.md
@@ -0,0 +1,10 @@
+To generate the interarrival times, I used the formula -(1/lambda)(ln(z)),
+where Z is a randomly generated number between 0 and 1, and 2 is lambda.
+
+To generate the service times which follow an exponential distribution, I used the formula -(1/lambda)(ln(1-y)), where y is a random number between 1 and 0.
+
+Part 2
+
+Lambda is 1/500, as we're expecting on average one failure for, 500 hours.
+
+
